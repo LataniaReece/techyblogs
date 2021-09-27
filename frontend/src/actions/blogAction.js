ae47eeb4c58352671e16dd345cef6f9b3b9159fc
LataniaReece <48932236+LataniaReece@users.noreply.github.com>
2021-09-27 15:49:07 -0400

    Update blogAction.js

--- frontend/src/actions/blogAction.js
+++ frontend/src/actions/blogAction.js
@@ -3,6 +3,9 @@ import {
     BLOG_LIST_FAIL,
     BLOG_LIST_REQUEST, 
     BLOG_LIST_SUCCESS,
+    BLOG_CREATE_REQUEST,
+    BLOG_CREATE_SUCCESS,
+    BLOG_CREATE_FAIL
 } from './actionTypes/blogTypes';
 
 export const getBlogs = () => async (dispatch) => {
@@ -19,6 +22,35 @@ export const getBlogs = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: BLOG_LIST_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        })
+    }
+}
+
+export const createBlog = () => async (dispatch, formData) => {
+    try {
+        dispatch({ type: BLOG_CREATE_REQUEST })
+
+//         const { userLogin: { userInfo } } = getState()
+
+//         const config = {
+//             headers: {
+//                 Authorization: `Bearer ${userInfo.token}`
+//             },
+//         }
+
+        const { data } = await axios.post('/api/blogs', formData);
+
+        dispatch({
+            type: BLOG_CREATE_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: BLOG_CREATE_FAIL,
             payload: error.response && error.response.data.message
                 ? error.response.data.message
                 : error.message
