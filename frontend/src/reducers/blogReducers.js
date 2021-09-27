3b37e12594469e71fab08719b1f0865bf481a46d
Reece <latania.reece@outlook.com>
2021-09-27 12:29:15 -0400

    updates

--- frontend/src/reducers/blogReducers.js
+++ frontend/src/reducers/blogReducers.js
@@ -1,5 +1,20 @@
-import { BLOG_CREATE_FAIL, BLOG_CREATE_REQUEST, BLOG_CREATE_RESET, BLOG_CREATE_SUCCESS } from "../actions/actionTypes/blogTypes"
-import { BLOG_LIST_FAIL, BLOG_LIST_REQUEST, BLOG_LIST_SUCCESS } from "./actionTypes/blogTypes"
+import {
+    BLOG_LIST_FAIL, 
+    BLOG_LIST_REQUEST, 
+    BLOG_LIST_SUCCESS, 
+    BLOG_CREATE_FAIL, 
+    BLOG_CREATE_REQUEST, 
+    BLOG_CREATE_RESET, 
+    BLOG_CREATE_SUCCESS, 
+    BLOG_UPDATE_REQUEST,
+    BLOG_UPDATE_SUCCESS,
+    BLOG_UPDATE_FAIL,
+    BLOG_UPDATE_RESET,
+    BLOG_DETAIL_REQUEST,
+    BLOG_DETAIL_SUCCESS,
+    BLOG_DETAIL_FAIL
+} from "../actions/actionTypes/blogTypes"
+
 
 
 export const blogListReducer = (state = { blogs: [] }, action) => {
@@ -18,6 +33,19 @@ export const blogListReducer = (state = { blogs: [] }, action) => {
     }
 }
 
+export const blogDetailReducer = (state = { blog: {} }, action) => {
+    switch (action.type) {
+        case BLOG_DETAIL_REQUEST:
+            return { ...state, loading: true }
+        case BLOG_DETAIL_SUCCESS:
+            return { loading: false, blog: action.payload }
+        case BLOG_DETAIL_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
 export const blogCreateReducer = (state = {}, action) => {
     switch (action.type) {
         case BLOG_CREATE_REQUEST:
@@ -27,6 +55,21 @@ export const blogCreateReducer = (state = {}, action) => {
         case BLOG_CREATE_FAIL:
             return { loading: false, error: action.payload }
         case BLOG_CREATE_RESET:
+            return {}
+        default:
+            return state
+    }
+}
+
+export const blogUpdateReducer = (state = { blog: {} }, action) => {
+    switch (action.type) {
+        case BLOG_UPDATE_REQUEST:
+            return { loading: true }
+        case BLOG_UPDATE_SUCCESS:
+            return { loading: false, success: true, blog: action.payload }
+        case BLOG_UPDATE_FAIL:
+            return { loading: false, error: action.payload }
+        case BLOG_UPDATE_RESET:
             return {}
         default:
             return state
