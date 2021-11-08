a0a23a02e3b78ab242a2b2bb33b865746e82ac8c
YOUR_USERNAME <latania.reece@outlook.com>
2021-11-07 19:19:41 -0500

    updates

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,8 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+const __dirname = path.resolve()
+
 const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl, {
